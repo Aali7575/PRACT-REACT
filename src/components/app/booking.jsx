0e3229a3bdb48eccfcd0955e3b3eb17bb8fe7491
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2024-04-29 02:15:16 +0200

    I have added date and time, still messy though

--- src/components/app/booking.jsx
+++ src/components/app/booking.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import TimePicker from 'react-time-picker'
+import { useState } from 'react';
 
 function Bookings() {
+    const [date, setDate] = useState(null); 
+    const [time, setTime] = useState(null);
   return (
     <div>
         <div className="h-[600px] bg-gray-100">
@@ -62,7 +68,22 @@ function Bookings() {
                 />
               </div>
             </div>
-            <div>
+            
+              <div className="block w-full px-4 py-3 placeholder-gray-500 border -gray-300 rounded-lg focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm caret-indigo-600">
+              <label>Date</label>
+              <DatePicker
+              selected={date}
+              onChange={(date) => setDate(date)}
+               />
+               </div>
+               <div className="block w-full px-4 py-3 placeholder-gray-500 border -gray-300 rounded-lg focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm caret-indigo-600">
+               <label>Time</label>
+               <TimePicker
+               selected={time} 
+               onChange={(time) => setTime(time)}
+               />
+              </div>
+              <div>
               <label htmlFor="" className="text-sm font-bold text-gray-900">
                 {" "}
                 Reason for booking{" "}
